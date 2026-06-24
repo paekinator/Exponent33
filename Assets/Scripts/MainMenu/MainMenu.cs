d0ca052bb5d9226b55e216f2a08c02c92f8bbf3a
MollyMenzies <molly.menzies@outlook.com>
2026-06-24 17:10:12 +1000

    Syncing Music and SFX Sliders

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,15 +1,47 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 // inspo: https://www.youtube.com/watch?v=B40xBPXK97A
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+    public void Start() {
+        LoadVolume();
+        MusicManager.Instance.PlayMusic("MainMenu");
+    }
     public void Play() {
         SceneManager.LoadScene("Game");
     }
 
     public void Quit() {
         Application.Quit();
+    }
+
+    public void UpdateMusicVolume(float volume) {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void UpdateSoundVolume(float volume) {
+        audioMixer.SetFloat("SFXVolume", volume);
+    }
+
+    public void SaveVolume() {
+        audioMixer.GetFloat("MusicVolume", out float musicVolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+
+        audioMixer.GetFloat("SFXVolume", out float SFXVolume);
+        PlayerPrefs.SetFloat("SFXVolume", SFXVolume);
+    }
+
+    public void LoadVolume() {
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
     }
 }
